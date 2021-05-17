f1b287b7aa1688fa278cc693a44ae11d639a9d66
Borza Peter <peter.borza@anaplan.com>
2021-05-17 15:20:57 +0300

    css files deleted

--- src/components/ImageFetch/ImageFetch.js
+++ src/components/ImageFetch/ImageFetch.js
@@ -1,43 +1,46 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
+import Button from "../Button/Button";
+// import useFetch from "../useFetch/useFetch";
 import styles from "./ImageFetch.module.scss";
 const ImageFetch = () => {
-  const [cats, setCats] = useState([]);
-  // const [newCat, setNewCat] = useState(false);
+    const [cats, setCats] = useState([]);
 
-  // const apiKey = "7d7c1821-4ae6-4c42-9247-443e9bad1402";
-  const url = `https://api.thecatapi.com/v1/images/search`;
+    // const apiKey = "7d7c1821-4ae6-4c42-9247-443e9bad1402";
+    const url = `https://api.thecatapi.com/v1/images/search`;
+    const fetchCats = async url => {
+        try {
+            const response = await fetch(url);
 
-  const fetchCats = async () => {
-    try {
-      const response = await fetch(url);
+            if (!response.ok) throw Error("not fetched in fetchCats");
 
-      if (!response.ok) throw Error("not fetched in fetchCats");
+            const data = await response.json();
 
-      const data = await response.json();
+            setCats(data);
 
-      setCats(data);
-      // setNewCat(false);
+            return data;
+        } catch (err) {
+            console.log(err, "cannot fetch data");
+        }
+    };
 
-      return data;
-    } catch (err) {
-      console.log(err, "cannot fetch data");
-    }
-  };
-
-  const catHandler = () => {
-    // setNewCat(true);
-    fetchCats();
-  };
-  return (
-    <>
-      <button onClick={catHandler}>Fetch Cat</button>
-      <div className={styles.catHolder}>
-        {cats.map((cat) => (
-          <img key={cat.id} src={cat.url} alt="" />
-        ))}
-      </div>
-    </>
-  );
+    const catHandler = () => {
+        fetchCats(url);
+    };
+    return (
+        // <>
+        //     <p>p</p>
+        //     {loading ? <p>Loading...</p> : "data arrived"}
+        //     {error && <p>{error.message}</p>}
+        // </>
+        <>
+            <Button handler={catHandler}>Click</Button>
+            <div className={styles.catHolder}>
+                {cats.map(cat => (
+                    <img key={cat.id} src={cat.url} alt="" />
+                ))}
+            </div>
+        </>
+    );
 };
 
 export default ImageFetch;

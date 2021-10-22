34556e8da94d34ce0fd48894e3d6445f8ce2877f
Borza Peter <borzapeter@yahoo.com>
2021-10-22 10:40:01 +0300

    0002 - slider intro, needs enhancement

--- src/components/Nav/links-provider.js
+++ src/components/Nav/links-provider.js
@@ -1,41 +1,49 @@
-import { createContext } from 'react';
+import { createContext } from "react";
 
-import ImageBox from '../ImageBox/ImageBox';
-import HomePage from '../HomePage/HomePage';
-import Cats from '../Cats/Cats';
-import ImageDetail from '../ImageDetail/ImageDetail';
+import ImageBox from "../ImageBox/ImageBox";
+import HomePage from "../HomePage/HomePage";
+import Cats from "../Cats/Cats";
+import ImageDetail from "../ImageDetail/ImageDetail";
+import SlideShow from "../SlideShow/SlideShow";
 
 export const LinkContext = createContext(null);
 
 export const LinkProvider = ({ children }) => {
 	const links = [
 		{
-			text: 'Home',
-			path: '/',
+			text: "Home",
+			path: "/",
 			exact: true,
 			component: HomePage,
 			id: 1,
 		},
 		{
-			text: 'Image-Box',
-			path: '/imagebox',
+			text: "Image-Box",
+			path: "/imagebox",
 			exact: true,
 			component: ImageBox,
 			id: 2,
 		},
 		{
-			text: 'Cats',
-			path: '/cats',
+			text: "Cats",
+			path: "/cats",
 			exact: false,
 			component: Cats,
 			id: 3,
 		},
 		{
+			text: "SlideShow",
+			path: "/slideshow",
+			exact: false,
+			component: SlideShow,
+			id: 4,
+		},
+		{
 			text: null,
-			path: '/imagebox/:id',
+			path: "/imagebox/:id",
 			exact: false,
 			component: ImageDetail,
-			id: 4,
+			id: 5,
 		},
 	];
 	return (
